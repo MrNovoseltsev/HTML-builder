5e9b430b6323c34a6f213a74f6915abf51c2688a
Big Buu <b-buu@Big-Buu.local>
2024-01-21 17:12:12 -0300

    feat: update task 4 synchronization for deleted files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,22 +3,40 @@ const path = require('path');
 const targetFolderPath = path.join(__dirname, 'files-copy');
 const sourceFolderPath = path.join(__dirname, 'files');
 
-fs.mkdir(targetFolderPath, (err) => {
+function copyDirectory() {
+  fs.mkdir(targetFolderPath, (err) => {
+    if(err) {
+      return console.log(err);
+    }
+  });
+  
+  fs.readdir(sourceFolderPath, (err, files) => { 
+    if (err) 
+      console.log(err); 
+    else {
+      files.forEach((file) => {
+        fs.copyFile(path.join(sourceFolderPath, file), path.join(targetFolderPath, file), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        })
+      })
+    }
+  });
+}
+
+fs.access(targetFolderPath, (err) => {
   if(err) {
-    return console.log(err);
+    console.log(err);
+  } else {
+    fs.rm(targetFolderPath, { recursive: true }, err => {
+      if (err) {
+        throw err;
+      }
+      copyDirectory();
+    });
   }
 });
 
-fs.readdir(sourceFolderPath, (err, files) => { 
-  if (err) 
-    console.log(err); 
-  else {
-    files.forEach((file) => {
-      fs.copyFile(path.join(sourceFolderPath, file), path.join(targetFolderPath, file), (err) => {
-        if (err) {
-          console.log(err);
-        }
-      })
-    })
-  }
-});
+copyDirectory();
+
